5458235c241fa0904b12ade72e9cf41d00d75b0e
Andyx34 <andreasolarte56@gmail.com>
2024-07-10 21:28:17 -0500

    add

---
+++ .idea/Main.kt
@@ -0,0 +1,18 @@
+//1
+fun main() {
+    val morningNotification=51
+    val eveningNotification=135
+
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+fun printNotificationSummary(numberOfMessages:Int) {
+    if(numberOfMessages < 100) {
+        println("tienes ${numberOfMessages} notificaciones")
+    }
+    else{
+        println("tienes +99 notificaciones")
+
+    }
+}
